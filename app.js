47090fc6d82a6243e27938f7d20392489610ddcf
plantGuy <noemai@spamalot.com>
2021-03-04 14:17:54 -0500

    deploy me

--- app.js
+++ app.js
@@ -4,44 +4,53 @@ const express = require("express");
 //const { registerHelper } = require("hbs");
 const port = process.env.PORT || 3000;
 const app = express();
-const xlsx = require("xlsx");
-const Mongo = require("mongodb");
-const MongoClient = Mongo.MongoClient;
+//const xlsx = require("xlsx");
+//const Mongo = require("mongodb");
+
+require("dotenv").config();
 
 app.get("/JSON/nativePlants", (req, res) => {
   //var search = JSON.parse(req.query.search);
-  var search = req.query;
+  var search = JSON.parse(req.query.search);
   Object.keys(search).forEach((element) => {
     if (search[element].match(/.*\.*/)) {
       console.log("like search detected");
-      search[element] = { $regex: search[element], $options: "i" };
+      //search[element] = { $regex: search[element], $options: "i" };
     }
   });
   console.log(search);
-  const Mongo = require("mongodb");
-  const MongoClient = Mongo.MongoClient;
+  //const Mongo = require("mongodb");
 
-  connectionURL = "mongodb://127.0.0.1:27017";
-  databaseName = "taskManager";
+  uri = `mongodb+srv://PlantsAdmin:${process.env.MongoDBPW}@cluster0.vikvy.mongodb.net/${process.env.MongoDB}?retryWrites=true&w=majority`;
+  databaseName = process.env.MongoDB;
 
-  MongoClient.connect(
-    connectionURL,
-    { useNewUrlParser: true },
-    (error, client) => {
-      if (error) {
-        return console.log(error);
-      } else {
-        const db = client.db(databaseName);
-        db.collection("plants")
-          .find(search)
-          .toArray((err, results) => {
-            if (results) {
-              res.send(results);
-            }
-          });
+  const MongoClient = require("mongodb").MongoClient;
+  //const uri = "mongodb+srv://PlantsAdmin:<password>@cluster0.vikvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect((err) => {
+    const collection = client.db(process.env.MongoDB).collection("Plants");
+    // perform actions on the collection object
+    collection.find(search).toArray((err, results) => {
+      if (results) {
+        res.send(results);
       }
-    }
-  );
+      client.close();
+    });
+  });
+
+  // client.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  //   //assert.equal(null, err);
+  //   if (error) {
+  //     return console.log(error);
+  //   } else {
+  //     const db = client.db(databaseName);
+  //     const collection = db.collection("Plants");
+  //
+  //   }
+  // });
   //   .find({})
 });
 
