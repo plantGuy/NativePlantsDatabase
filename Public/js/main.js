631f55494875797404137539c0c1dfd43d6025b8
plantGuy <noemai@spamalot.com>
2021-03-04 17:03:37 -0500

    added example page

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,22 +1,40 @@
 function getPlants() {
   var name = $(ScientificName).val();
+  var strSciName = name;
+
   $.ajax({
     //http://localhost:3000/JSON/nativePlants?search={%22Species%22:%22Asclepias%22}
     url: "/JSON/nativePlants",
     type: "GET",
     data: {
-      search: { strSciName: name },
+      search: JSON.stringify({ strSciName }),
     },
     error: function (error) {
       console.log(error);
     },
     success: function (data) {
-      console.log(data);
-      $("#strCommonName").val(data.strCommonName);
-      $("#flgNativeTo").val(data.flgNativeTo);
-      $("#strSpecialUses").val(data.strSpecialUses);
-      $("#strPlantingRecommendations").val(data.strPlantingRecommendations);
-      $("#strGrowthPatterns").val(data.strGrowthPatterns);
+      console.log(data[0].strCommonName);
+      $("#strCommonName").html(makeList(data[0].strCommonName));
+      $("#flgNativeTo").html(makeList(data[0].flgNativeTo));
+      $("#strSpecialUses").html(makeList(data[0].strSpecialUses));
+      $("#strPlantingRecommendations").html(
+        makeList(data[0].strPlantingRecommendations)
+      );
+      $("#strGrowthPatterns").html(makeList(data[0].strGrowthPatterns));
     },
   });
+}
+
+function makeList(object) {
+  var result = "";
+  if (Array.isArray(object)) {
+    result = result + "<UL>";
+    object.forEach((element) => {
+      result = result + `<li>${element}</li>`;
+    });
+    result = result + "</UL>";
+  } else {
+    result = `<P>${object}</p>`;
+  }
+  return result;
 }
--- src/app.js
+++ src/app.js
@@ -12,35 +12,39 @@ app.get("/", (req, res) => {
 
 app.get("/JSON/nativePlants", (req, res) => {
   //var search = JSON.parse(req.query.search);
-  var search = JSON.parse(req.query.search);
-  Object.keys(search).forEach((element) => {
-    if (search[element].match(/.*\.*/)) {
-      console.log("like search detected");
-      //search[element] = { $regex: search[element], $options: "i" };
-    }
-  });
-  console.log(search);
-  //const Mongo = require("mongodb");
-
-  uri = `mongodb+srv://PlantsAdmin:${process.env.MongoDBPW}@cluster0.vikvy.mongodb.net/${process.env.MongoDB}?retryWrites=true&w=majority`;
-  databaseName = process.env.MongoDB;
-
-  const MongoClient = require("mongodb").MongoClient;
-  //const uri = "mongodb+srv://PlantsAdmin:<password>@cluster0.vikvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  client.connect((err) => {
-    const collection = client.db(process.env.MongoDB).collection("Plants");
-    // perform actions on the collection object
-    collection.find(search).toArray((err, results) => {
-      if (results) {
-        res.send(results);
+  try {
+    var search = JSON.parse(req.query.search);
+    Object.keys(search).forEach((element) => {
+      if (search[element].match(/.*\.*/)) {
+        console.log("like search detected");
+        //search[element] = { $regex: search[element], $options: "i" };
       }
-      client.close();
     });
-  });
+    console.log(search);
+    //const Mongo = require("mongodb");
+
+    uri = `mongodb+srv://PlantsAdmin:${process.env.MongoDBPW}@cluster0.vikvy.mongodb.net/${process.env.MongoDB}?retryWrites=true&w=majority`;
+    databaseName = process.env.MongoDB;
+
+    const MongoClient = require("mongodb").MongoClient;
+    //const uri = "mongodb+srv://PlantsAdmin:<password>@cluster0.vikvy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    client.connect((err) => {
+      const collection = client.db(process.env.MongoDB).collection("Plants");
+      // perform actions on the collection object
+      collection.find(search).toArray((err, results) => {
+        if (results) {
+          res.send(results);
+        }
+        client.close();
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
